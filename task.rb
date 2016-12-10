c5d4c750d8f59faa3617e8de1bcf34bbd6379cc2
FaIzon <dimer90@gmail.com>
2016-12-10 10:37:47 +0700

    Notepad v 0.1

--- task.rb
+++ task.rb
@@ -1,3 +1,4 @@
+require 'date'
 
 class Task  < Post
 
@@ -9,10 +10,20 @@ class Task  < Post
   end
 
   def read_from_console
+    puts "Что надо сделать?"
+    @text = STDIN.gets.chomp
 
+    puts "К какому числу? ДД.ММ.ГГГГ "
+    input  = STDIN.gets.chomp
+
+    @due_date = Date.parse(input)
   end
 
   def to_strings
-    # todo
+    time_string = "Создано: #{@created_at.strftime("%Y.%m.%d, %H:%M:%S")}  \n\r \n\r"
+    deadline = "Крайний срок #{@due_date}"
+
+    return [deadline, @text, time_string]
+
   end
 end
