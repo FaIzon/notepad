3c0ac4fa8fa38d8710525a6ad1eaf91529e9b86c
FaIzon <dimer90@gmail.com>
2017-01-02 20:25:27 +0700

    Add SQLite to notepad

--- new_post.rb
+++ new_post.rb
@@ -3,13 +3,15 @@ require_relative 'link.rb'
 require_relative 'task.rb'
 require_relative 'memo.rb'
 
+puts "Версия с SQLite"
 puts "Запишите что-нибудь?"
 
-choices = Post.post_types
+choices = Post.post_types.keys
 
 choice = -1
 
 until choice >= 0 && choice < choices.size
+
   choices.each_with_index do |type, index|
     puts "\t#{index}. #{type}"
   end
@@ -17,10 +19,10 @@ until choice >= 0 && choice < choices.size
   choice = STDIN.gets.chomp.to_i
 end
 
-entry = Post.create(choice)
+entry = Post.create(choices[choice])
 
 entry.read_from_console
 
-entry.save
+entry.save_to_db
 
 
